38101befcd313b57f3e38f1a76a92e6f18860e00
Evgeny <buze@ya.ru>
2021-03-29 12:37:23 +0800

    Initial Commit

---
+++ CircularIndicator/CircularIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularIndicatorApp.swift
+//  CircularIndicator
+//
+//  Created by Evgeny on 29.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct CircularIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
