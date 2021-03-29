7deba3aca0b0482dcc0d696450a3abefc62ceb6e
Evgeny <buze@ya.ru>
2021-03-29 12:58:35 +0800

    add code
    add view, shape and buttons

---
+++ CircularIndicator/CircularIndicatorShape.swift
@@ -0,0 +1,8 @@
+//
+//  CircularIndicatorShape.swift
+//  CircularIndicator
+//
+//  Created by Evgeny on 29.03.2021.
+//
+
+import Foundation
---
+++ CircularIndicator/CircularIndicatorView.swift
@@ -0,0 +1,8 @@
+//
+//  CircularIndicatorView.swift
+//  CircularIndicator
+//
+//  Created by Evgeny on 29.03.2021.
+//
+
+import Foundation
